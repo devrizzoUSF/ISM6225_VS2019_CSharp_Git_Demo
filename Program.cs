55239afa132b974d67e44f023895b9102afcec67
Devon Rizzo <devonnrizzo@gmail.com>
2020-01-14 20:33:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM6225_VS2019_CSharp_Git_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
